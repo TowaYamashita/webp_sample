7a0b95932a39953990dcf1f69277a123e1d8d4b0
TowaYamashita <Confirm.Apps.Develop@gmail.com>
2023-12-16 10:54:26 +0900

    webp形式の画像を表示できるかの確認

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,12 +26,102 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const loadingImagePath = 'assets/images/loading.gif';
+    const webpImagePath = 'assets/images/example.webp';
+    const webpImageUrl = 'https://www.gstatic.com/webp/gallery/4.webp';
+
+    const webpImageWidth = 1024;
+    const webpImageheight = 772;
+
     return Scaffold(
       appBar: AppBar(),
-      body: const Center(
-        child: Text(
-          'Hello',
-        ),
+      body: ListView(
+        children: [
+          ListTile(
+            title: const Text('Image.asset'),
+            trailing: const Icon(Icons.image),
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (_) {
+                  return Dialog(
+                    child: Image.asset(webpImagePath),
+                  );
+                },
+              );
+            },
+          ),
+          ListTile(
+            title: const Text('Image.network'),
+            trailing: const Icon(Icons.image),
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (_) {
+                  return Dialog(
+                    child: Image.network(
+                      webpImageUrl,
+                    ),
+                  );
+                },
+              );
+            },
+          ),
+          ListTile(
+            title: const Text('FadeInImage.assetNetwork'),
+            trailing: const Icon(Icons.image),
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (_) {
+                  return LayoutBuilder(builder: (_, c) {
+                    final maxWidth = c.maxWidth;
+                    final width = maxWidth * 0.8;
+                    final height = webpImageheight * width / webpImageWidth;
+
+                    return Dialog(
+                      child: FadeInImage.assetNetwork(
+                        placeholder: loadingImagePath,
+                        image: webpImageUrl,
+                        width: width,
+                        height: height,
+                      ),
+                    );
+                  });
+                },
+              );
+            },
+          ),
+          ListTile(
+            title: const Text('CachedNetworkImage'),
+            trailing: const Icon(Icons.image),
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (_) {
+                  return LayoutBuilder(builder: (_, c) {
+                    final maxWidth = c.maxWidth;
+                    final width = maxWidth * 0.8;
+                    final height = webpImageheight * width / webpImageWidth;
+
+                    return Dialog(
+                      child: CachedNetworkImage(
+                        imageUrl: webpImageUrl,
+                        width: width,
+                        height: height,
+                        placeholder: (_, __) {
+                          return const Center(
+                            child: CircularProgressIndicator.adaptive(),
+                          );
+                        },
+                      ),
+                    );
+                  });
+                },
+              );
+            },
+          ),
+        ],
       ),
     );
   }
